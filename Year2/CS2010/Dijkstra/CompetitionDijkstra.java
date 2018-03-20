efb9ddce1f682cfca17c2dec55755682144e3642
Jakub Slowinski <32519034+sickfila@users.noreply.github.com>
2018-03-20 14:04:16 +0000

    Add files via upload

---
+++ Year2/CS2010/Dijkstra/CompetitionDijkstra.java
@@ -0,0 +1,44 @@
+/*
+ * A Contest to Meet (ACM) is a reality TV contest that sets three contestants at three random
+ * city intersections. In order to win, the three contestants need all to meet at any intersection
+ * of the city as fast as possible.
+ * It should be clear that the contestants may arrive at the intersections at different times, in
+ * which case, the first to arrive can wait until the others arrive.
+ * From an estimated walking speed for each one of the three contestants, ACM wants to determine the
+ * minimum time that a live TV broadcast should last to cover their journey regardless of the contestants’
+ * initial positions and the intersection they finally meet. You are hired to help ACM answer this question.
+ * You may assume the following:
+ *     Each contestant walks at a given estimated speed.
+ *     The city is a collection of intersections in which some pairs are connected by one-way
+ * streets that the contestants can use to traverse the city.
+ *
+ * This class implements the competition using Dijkstra's algorithm
+ */
+
+public class CompetitionDijkstra
+{
+
+  /**
+   * @param filename:
+   *          A filename containing the details of the city road network
+   * @param sA,
+   *          sB, sC: speeds for 3 contestants
+   */
+  CompetitionDijkstra(String filename, int sA, int sB, int sC)
+  {
+
+    // TODO
+  }
+
+  /**
+   * @return int: minimum minutes that will pass before the three contestants
+   *         can meet
+   */
+  public int timeRequiredforCompetition()
+  {
+
+    // TO DO
+    return -1;
+  }
+
+}
---
+++ Year2/CS2010/Dijkstra/CompetitionTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompetitionTest
+{
+
+  @Test
+  public void testDijkstraConstructor()
+  {
+
+    // TODO
+  }
+
+  @Test
+  public void testFWConstructor()
+  {
+    // TODO
+  }
+
+  // TODO - more tests
+
+}
